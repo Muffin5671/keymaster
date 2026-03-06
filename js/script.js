f430c60085f80ee43f287b61015bff65aacab748
Muffin5671 <213143890+Muffin5671@users.noreply.github.com>
2026-03-05 22:26:34 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 let sfx = new Audio('sounds/achievement.mp3');
 
 async function readResponses() {
-  let x = await fetch('../data/keymasterResponses.json');
+  let x = await fetch('data/keymasterResponses.json');
   let y = await x.json();
   y.forEach((element) => console.log(element));
 }
